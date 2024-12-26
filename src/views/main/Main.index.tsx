96b4b4bb27b50291c0acf8f300083b9d9cfc72da
Gísli Bragi Hjartarson <gislib@Heavy-Mobile.local>
2024-12-26 23:19:16 +0000

    pre animations

--- src/views/main/Main.index.tsx
+++ src/views/main/Main.index.tsx
@@ -1,24 +1,26 @@
 import React, { useState, useEffect } from "react";
-import { View, Text, TouchableOpacity } from "react-native";
-import { useGetWeatherQuery, useGetForecastQuery } from "../../services/weatherApi";
+import { View, Text, TouchableOpacity, Image } from "react-native";
+import { useGetForecastQuery } from "../../services/weatherApi";
 import * as Location from "expo-location";
 
 import styles from "./Main.styles";
 
+import { LinearGradient } from 'expo-linear-gradient';
+
+
 const Main = () => {
-  const [queryLocation, setQueryLocation] = useState<string>(""); 
-  const { data: weatherData, error: weatherError, isLoading: isWeatherLoading } = useGetWeatherQuery(queryLocation, {
+  const [queryLocation, setQueryLocation] = useState<string>("");
+  const {
+    data: forecastData,
+    error: forecastError,
+    isLoading: isForecastLoading,
+  } = useGetForecastQuery(queryLocation, {
     skip: !queryLocation,
   });
-  
-  const { data: forecastData, error: forecastError, isLoading: isForecastLoading } = useGetForecastQuery(queryLocation, {
-    skip: !queryLocation,
-  });  
   const [location, setLocation] = useState<Location.LocationObject | null>(
     null
   );
   const [errorMsg, setErrorMsg] = useState<string | null>(null);
-  const [coordinateString, setCoordinateString] = useState<string | null>(null);
 
   useEffect(() => {
     async function getCurrentLocation() {
@@ -33,36 +35,20 @@ const Main = () => {
 
       const coordinates =
         location?.coords.latitude + "," + location?.coords.longitude;
-      setCoordinateString(coordinates);
 
-      setQueryLocation(coordinates); 
+      setQueryLocation(coordinates);
     }
 
     getCurrentLocation();
   }, []);
 
   return (
-    <View style={styles.container}>
-      {isWeatherLoading && <Text>Loading weather...</Text>}
-      {weatherError && (
-        <Text>
-          Weather Error:
-          {"status" in weatherError
-            ? weatherError.status
-            : "message" in weatherError
-            ? weatherError.message
-            : "data" in weatherError && typeof weatherError.data === "string"
-            ? weatherError.data
-            : "An unknown error occurred"}
-        </Text>
-      )}
-
-      {weatherData && <Text>Weather: {weatherData.current.condition.text}</Text>}
-
+    <LinearGradient colors={["#4c669f", "#3b5998", "#192f6a"]}
+    style={styles.container}>
       {isForecastLoading && <Text>Loading forecast...</Text>}
       {forecastError && (
         <Text>
-          Forecast Error:
+          Error:
           {"status" in forecastError
             ? forecastError.status
             : "message" in forecastError
@@ -73,12 +59,28 @@ const Main = () => {
         </Text>
       )}
 
-      {forecastData && <Text>Forecast: {forecastData.forecast.forecastday[0].day.condition.text}</Text>}
+      {forecastData && (
+        <>
+          <Text style={styles.location}>{forecastData.location.name}</Text>
+          <Text style={styles.temperature}>{forecastData.current.temp_c}°C</Text>
+          <Text style={styles.feelslike}>Feels like: {forecastData.current.feelslike_c}°C</Text>
+          <Text>Current Weather: {forecastData.current.condition.text}</Text>
+          <Image
+            source={{
+              uri: `https:${forecastData.current.condition.icon}`,
+            }}
+            style={{ width: 50, height: 50 }}
+          />
+          <Text>
+            Forecast: {forecastData.forecast.forecastday[0].day.condition.text}
+          </Text>
+        </>
+      )}
 
       <TouchableOpacity onPress={() => console.log(forecastData)}>
         <Text>Press me</Text>
       </TouchableOpacity>
-    </View>
+    </LinearGradient>
   );
 };
 
--- src/views/main/Main.styles.tsx
+++ src/views/main/Main.styles.tsx
@@ -3,7 +3,22 @@ import { StyleSheet } from "react-native";
 export default StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: "center",
     alignItems: "center",
+    
+  },
+  text: {
+    fontSize: 40,
+    fontWeight: "bold",
+  },
+  location: {
+    fontSize: 30,
+    marginTop: 70,
+  },
+  temperature: {
+    fontSize: 60,
+  },
+  feelslike: {
+    fontSize: 20,
+    color: "gray",
   },
 });
